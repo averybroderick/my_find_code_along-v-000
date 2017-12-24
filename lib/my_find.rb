7eaf84794eabc2571c5f16211b81a4b5e1d231fe
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-24 16:28:09 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,19 +1,11 @@
 require 'pry'
-#
-# def my_find(collection)
-#   i = 0
-#   while i < collection.length
-#     if yield(collection[i])
-#       return collection[i]
-#     end
-#     i+=1
-#   end
-# end
 
 def my_find(collection)
   i = 0
   while i < collection.length
-    return collection[i] if yield(collection[i])
-    i = i + 1
+    if yield(collection[i])
+      return collection[i]
+      i+=1
+    end
   end
 end
